2685311725312646f044622415953da0be3f284c
Uday BK <uday.nbausj@gmail.com>
2018-08-07 00:01:25 +0530

    Create support_vector_machine.py
    Sample data is trained and tested on Support vector machine algorithm. Accuracy of prediction is calculated.

---
+++ support_vector_machine.py
@@ -0,0 +1,15 @@
+import numpy
+import scipy
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+from prep_terrain_data import makeTerrainData
+
+features_train,labels_train,features_test,labels_test = makeTerrainData()
+
+classifier = svm.SVC()
+classifier.fit(features_train,labels_train)
+
+pred = classifier.predict(features_test)
+
+print(classifier.score(features_test,labels_test))
