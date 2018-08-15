b2e966c283172dd30d6603cd3731f24f70d4f563
Uday BK <uday.nbausj@gmail.com>
2018-08-15 18:06:17 +0530

    Random forest on terrain data

---
+++ random_forest_terrain.py
@@ -0,0 +1,19 @@
+import numpy
+import scipy
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from prep_terrain_data import makeTerrainData
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+features_train,labels_train,features_test,labels_test = makeTerrainData()
+
+classifier = RandomForestClassifier()
+classifier.fit(features_train,labels_train)
+
+pred = classifier.predict(features_test)
+
+print(classifier.score(features_test,labels_test))
